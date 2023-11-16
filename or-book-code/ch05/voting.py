e70a9c22b7efe8b0717bb6cbfab32e1b1a7b91b8
jameydev <jybryce4@gmail.com>
2023-11-16 15:14:28 -0600

    if-else example

---
+++ or-book-code/ch05/voting.py
@@ -0,0 +1,7 @@
+age = 19
+if age >= 18:
+    print("You can vote.")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
