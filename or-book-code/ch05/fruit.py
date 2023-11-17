5ea67ab41e901e28ad3f5ccd064df825727ff2eb
jameydev <jybryce4@gmail.com>
2023-11-17 08:07:50 -0600

    favorite fruits exercise chapter 5

---
+++ or-book-code/ch05/fruit.py
@@ -0,0 +1,14 @@
+favorite_fruits = ['apple', 'banana', 'strawberry']
+
+if 'apple' in favorite_fruits:
+    print("You really like apples!")
+if 'banana' in favorite_fruits:
+    print("You really like bananas!")
+if 'strawberry' in favorite_fruits:
+    print("You really like strawberries!")
+
+# Won't display anything uiless you add it to the list
+if 'kiwi' in favorite_fruits:
+    print("You really like kiwis!")
+if 'orange' in favorite_fruits:
+    print("You really like oranges!")
