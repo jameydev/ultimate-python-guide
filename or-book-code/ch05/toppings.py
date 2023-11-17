4a757d5d9f4f1a0d3dd76933c8b72655e32848b6
jameydev <jybryce4@gmail.com>
2023-11-17 08:16:23 -0600

    updated toppings example from chapter 5

--- or-book-code/ch05/toppings.py
+++ or-book-code/ch05/toppings.py
@@ -1,4 +1,26 @@
-requested_topping = 'mushrooms'
+# requested_topping = 'mushrooms'
 
-if requested_topping != 'anchovies':
-    print("Hold the anchovies!")
+# if requested_topping != 'anchovies':
+#     print("Hold the anchovies!")
+
+available_toppings = ['mushrooms', 'olives', 'green peppers',
+                      'pepperoni', 'pineapple', 'extra cheese']
+
+requested_toppings = ['mushrooms', 'extra cheese', 'pepperoni', 'green peppers']
+# requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        # print(f"Adding {requested_topping}.")
+        # if requested_topping == 'green peppers':
+        #     print("Sorry, we are out of green peppers right now.")
+        # else:
+        #     print(f"Adding {requested_topping}.")
+        if requested_topping in available_toppings:
+            print(f"Adding {requested_topping}.")
+        else:
+            print(f"Sorry, we don't have {requested_topping}.")
+else:
+    print("Are you sure you want a plain pizza?")
+    
+# print("\nFinished making your pizza!")
