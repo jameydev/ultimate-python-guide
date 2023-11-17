39ce766a91d2c462c0248dc442d63585b4fe65bb
jameydev <jybryce4@gmail.com>
2023-11-17 08:03:56 -0600

    alien colors exercise from chapter 5 of oreilly book

---
+++ or-book-code/ch05/alien_colors1.py
@@ -0,0 +1,8 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
+# no output
+alien_color = 'red'
+if alien_color == 'green':
+    print("You just earned 5 points!")
