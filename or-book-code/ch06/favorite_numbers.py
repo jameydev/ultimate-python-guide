05724fab44307fea7f6ff9af0c7c36c8d2d19759
jameydev <jybryce4@gmail.com>
2023-11-22 13:31:51 -0600

    favorite numbers dictionary loop example

---
+++ or-book-code/ch06/favorite_numbers.py
@@ -0,0 +1,20 @@
+favorite_numbers = {
+    'jen': 7,
+    'sarah': 3,
+    'edward': 9,
+    'phil': 7,
+    'jim': 2,
+    'tom': 5,
+    'jerry': 3,
+    'mike': 5,
+    'andrew': 6,
+    'carl': 4,
+    'james': 6,
+    'jane': 6,
+    'eric': 8,
+    'john': 8,
+    'joe': 4,
+}
+
+for name, number in favorite_numbers.items():
+    print(f"{name.title()}'s favorite number is {number}.")
