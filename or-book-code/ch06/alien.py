4528728023287ed2f34a5e36d222772d16940877
jameydev <jybryce4@gmail.com>
2023-11-26 08:35:43 -0600

    alien - dictionary example - updates

--- or-book-code/ch06/alien.py
+++ or-book-code/ch06/alien.py
@@ -1,10 +1,10 @@
 alien_0 = {'color': 'green', 'points': 5}
-print(alien_0['color'])
-print(alien_0['points'])
+print(f"The alien is colored {alien_0['color']}.")
+print(f"The alien is worth {alien_0['points']} points.")
 
 # A new one for the next example
 alien_0 = {'color': 'green'}
-print(f"The alien is {alien_0['color']}.")
+print(f"\nThe new alien is {alien_0['color']}.")
 
 alien_0['color'] = 'yellow'
 print(f"The alien is now {alien_0['color']}.")
@@ -29,7 +29,14 @@ print(f"New position: {alien_0['x_position']}")
 
 # Remove a key-value pair
 alien_0 = {'color': 'green', 'points': 5}
-print(alien_0)
+print(f"\nBefore deleting points: {alien_0}")
 
 del alien_0['points']
-print(alien_0)
+print(f"\nAfter deleting points: {alien_0}")
+
+# Start with an empty dictionary
+alien_0 = {}
+print(f"\nStarted with an empty dictionary: {alien_0}")
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+print(f"Final alien_0: {alien_0}")
